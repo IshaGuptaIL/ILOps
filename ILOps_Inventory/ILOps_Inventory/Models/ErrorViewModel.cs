b12fefa15f96ed723e0df3e262ca86605e91e7e6
IshaGuptaIL <igupta@ironlogic.ca>
2026-02-02 14:25:53 +0530

    Delete ILOps_Inventory directory

--- ILOps_Inventory/ILOps_Inventory/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ILOps_Inventory.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
